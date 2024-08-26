eb035ddb8e38e332c1c2c5d43e8562da6aba4a69
unknown <shinichikudo1st>
2024-08-26 19:34:12 +0800

    responsive mobile login and signup, pending retrieve appraisal

--- app/components/universal/ctuBackground.jsx
+++ app/components/universal/ctuBackground.jsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 import background from "@/public/background.png";
 
 const Background = () => {
-  const responsive = "lg:top-20 lg:h-[80vh] lg:translate-y-[-5px]";
+  const responsive =
+    "h-[50vh] top-[10%] lg:top-20 lg:h-[80vh] lg:translate-y-[-5px]";
 
   return (
     <Image
--- app/components/universal/navbar.jsx
+++ app/components/universal/navbar.jsx
@@ -1,15 +1,17 @@
 const Navbar = ({ login, signup }) => {
-  const responsive = "lg:h-[10vh] 2xl:h-[8vh]";
+  const responsive = "h-[10vh] lg:h-[10vh] 2xl:h-[8vh]";
 
   const buttonProperty =
-    "w-[150px] text-[15pt] font-bold text-[#062341] rounded-[10px] ";
+    "w-[150px] text-[10pt] 2xl:text-[15pt] font-bold text-[#062341] rounded-[10px] ";
 
   return (
     <nav
       className={`absolute w-full flex items-center justify-between px-5 ${responsive}`}
     >
-      <h1 className=" text-[#062341] font-bold text-[25pt]">Guiding Path</h1>
-      <div className="flex w-[30%] h-[10vh] justify-center gap-3">
+      <h1 className=" text-[#062341] font-bold text-[15pt] 2xl:text-[25pt]">
+        Guiding Path
+      </h1>
+      <div className="flex w-[30%] 2xl:h-[10vh] justify-center gap-3">
         <button className={buttonProperty} onClick={login}>
           Login
         </button>
