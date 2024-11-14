7ebed28c75e6601e684b45c72659219bf7788c2e
unknown <iam180622@gmail.com>
2024-11-14 19:20:23 +0800

    cancel referral request

---
+++ app/api/cancelReferral/route.js
@@ -0,0 +1,35 @@
+import { getSession } from "@/app/utils/authentication";
+import prisma from "@/app/utils/prisma";
+import { NextResponse } from "next/server";
+
+export async function PUT(request) {
+  const url = new URL(request.url);
+  const id = url.searchParams.get("id");
+
+  const { sessionData } = await getSession();
+
+  if (!sessionData) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+
+  try {
+    await prisma.referrals.update({
+      where: {
+        referral_id: parseInt(id),
+      },
+      data: {
+        status: "cancelled",
+      },
+    });
+
+    return NextResponse.json(
+      { message: "Referral cancelled successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
