de534432bccbd986cb97beb3e40298ddd752af67
Shinichi Kudo <iam180622@gmail.com>
2024-12-03 07:36:05 +0800

    middleware update / remove if errors occur

--- middleware.js
+++ middleware.js
@@ -14,7 +14,7 @@ const securityHeaders = {
   "Referrer-Policy": "strict-origin-when-cross-origin",
   "X-XSS-Protection": "1; mode=block",
   "Content-Security-Policy":
-    "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline';",
+    "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https: blob:;",
   "Strict-Transport-Security": "max-age=31536000; includeSubDomains",
 };
 
