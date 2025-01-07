3eecc7d09250184415d964f69c728b8ca0a52d3a
unknown <iam180622@gmail.com>
2025-01-07 21:14:40 +0800

    changed edit grade level

--- app/components/universal/loginModal.jsx
+++ app/components/universal/loginModal.jsx
@@ -499,11 +499,11 @@ const LoginModal = ({
                         <option disabled value="">
                           Select Year Level
                         </option>
-                        <option value="1">1st Year</option>
-                        <option value="2">2nd Year</option>
-                        <option value="3">3rd Year</option>
-                        <option value="4">4th Year</option>
-                        <option value="5">5th Year</option>
+                        <option value="1st Year">1st Year</option>
+                        <option value="2nd Year">2nd Year</option>
+                        <option value="3rd Year">3rd Year</option>
+                        <option value="4th Year">4th Year</option>
+                        <option value="5th Year">5th Year</option>
                       </select>
                     </div>
                   </motion.div>
